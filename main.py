8985bec3ed42bf6726e11847cf7268396af19f03
Alexandr_Popov <Reorin262@gmail.com>
2022-10-06 20:36:33 +0300

    fin

---
+++ main.py
@@ -0,0 +1,12 @@
+from application import salary
+from application.db.people import get_employees
+
+
+if __name__ == '__main__':
+        print(get_employees())
+        salary.salculate_salary(30000, 25000)
+
+
+
+
+
